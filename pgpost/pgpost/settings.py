0ba413e96213a56c2726d5891d3e3ca2ed1b66c7
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-30 09:34:50 +0200

    admin init

--- pgpost/pgpost/settings.py
+++ pgpost/pgpost/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'securemsg.apps.SecuremsgConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
