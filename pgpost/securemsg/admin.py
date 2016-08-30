e0b764b93ec629cee7ec31a84ce4bf3c3cd72db6
mronkain <mrnk@iki.fi>
2016-08-30 22:45:57 +0300

    add KeyMaster, DataRequest models and migrations

--- pgpost/securemsg/admin.py
+++ pgpost/securemsg/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import PublicKey
+from .models import PublicKey, KeyMaster, DataRequest
 
 admin.site.register(PublicKey)
+admin.site.register(KeyMaster)
+admin.site.register(DataRequest)
