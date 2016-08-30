50f023347d9f35dd2fc56e24bcba3f7496f532c7
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-30 09:34:50 +0200

    admin init

--- pgpost/securemsg/models.py
+++ pgpost/securemsg/models.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class PublicKey(models.Model):
+    public_key = models.CharField(max_length=500)
+    public_key = models.CharField(max_length=100)
