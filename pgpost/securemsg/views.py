8fe769c40ba80a8de192ba856a3ed256ccc29b3b
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-30 10:03:17 +0200

    genkey form draft

--- pgpost/securemsg/views.py
+++ pgpost/securemsg/views.py
@@ -5,6 +5,11 @@ from django.template import loader
 def index(request):
     return HttpResponse("Hello, world. You're at the securemsg index.")
 
+def genkey(request):
+    template = loader.get_template('securemsg/genkey.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
 def addkey(request):
     template = loader.get_template('securemsg/addkey.html')
     context = {}
