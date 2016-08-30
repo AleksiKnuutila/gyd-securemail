227b3fa0a3b22d4806bfa841a92606f3e301a52f
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-30 10:03:17 +0200

    genkey form draft

--- pgpost/securemsg/urls.py
+++ pgpost/securemsg/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'securemsg'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^addkey$', views.addkey, name='addkey'),
+    url(r'^genkey$', views.genkey, name='genkey'),
 ]
