e8e3e65f5bebbebb2e0b7854cef522771fcf8771
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-30 09:49:48 +0200

    addkey view

--- pgpost/securemsg/views.py
+++ pgpost/securemsg/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the securemsg index.")
+
+def addkey(request):
+    template = loader.get_template('securemsg/addkey.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
