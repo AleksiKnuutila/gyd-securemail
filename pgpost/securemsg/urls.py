73a0204ad891eff0151e70cc1e25bfad84c7ba21
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-31 18:21:12 +0300

    add first user journey endpoints

--- pgpost/securemsg/urls.py
+++ pgpost/securemsg/urls.py
@@ -6,7 +6,6 @@ app_name = 'securemsg'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^addkey$', views.addkey, name='addkey'),
     url(r'^genkey$', views.genkey, name='genkey'),
     url(r'^encryptfile$', views.encryptfile, name='encryptfile'),
     url(r'^addencrypted$', views.addencrypted, name='addencrypted'),
@@ -14,5 +13,7 @@ urlpatterns = [
     url(r'^decrypt_index$', views.decrypt_index, name='decrypt_index'),
     url(r'^json_get_publickey', views.json_get_publickey, name='json_get_publickey'),
     url(r'^json_get_datareq', views.json_get_datareq, name='json_get_datareq'),
-    url(r'^json_addencrypted', views.json_addencrypted, name='json_addencrypted'),
+    url(r'^json_addkeymaster$', views.addkey, name='addkey'),
+    url(r'^json_confirmemail$', views.json_addencrypted, name='json_addencrypted'),
+    url(r'^json_addkey$', views.addkey, name='addkey'),
 ]
