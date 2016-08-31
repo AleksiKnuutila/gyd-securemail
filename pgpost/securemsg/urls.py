07f94359aa54e68ce55846744f39808d236c5f16
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-31 21:32:15 +0300

    remove unnecessary stuff from urls.py

--- pgpost/securemsg/urls.py
+++ pgpost/securemsg/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'securemsg'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+#    url(r'^encryptfile$', views.encryptfile, name='encryptfile'),
+    url(r'^$', views.genkey, name='index'),
     url(r'^genkey$', views.genkey, name='genkey'),
-    url(r'^encryptfile$', views.encryptfile, name='encryptfile'),
     url(r'^sendfile_index$', views.sendfile_index, name='sendfile_index'),
     url(r'^decrypt_index$', views.decrypt_index, name='decrypt_index'),
     url(r'^json_get_publickey', views.json_get_publickey, name='json_get_publickey'),
